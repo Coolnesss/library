46b022fc55095e896369cc8ec49c55e3c07e8233
Chang <chang.rajani@gmail.com>
2017-07-08 14:46:55 +0300

    minor fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,8 +14,7 @@ module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "toast #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
-        concat(content_tag(:button, class: 'btn-clear float-right', data: { dismiss: 'alert' }) do
-          concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
+        concat(content_tag(:button, class: 'btn btn-clear float-right', data: { dismiss: 'alert' }) do
           concat content_tag(:span, 'Close', class: 'sr-only')
         end)
         concat message
