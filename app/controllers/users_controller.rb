8cb82d63ae550e77480a6b2c5614bf6d8cc42c02
Chang <chang.rajani@gmail.com>
2019-11-18 21:38:54 +0200

    Also downcase emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    @user.email = @user.email.strip
+    @user.email = @user.email.strip.downcase
 
     if @user.save
       redirect_to login_path, notice: 'User created succesfully. Wait for an admin to activate your account.'
